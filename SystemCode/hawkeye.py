fe7b50fafb9b47109507870e474ccd5992a23a18
DebadityaPal <debaditya.pal6@gmail.com>
2020-12-13 14:59:44 +0530

    Model Threshold modified

--- SystemCode/hawkeye.py
+++ SystemCode/hawkeye.py
@@ -12,7 +12,7 @@ import time
 hit = False
 hit_sensitivity = 0
 # If zero written here captures from laptop camera otherwise we can also pass in a video file here
-device_id = './test.mp4'
+device_id = './input.mp4'
 cap = cv2.VideoCapture(device_id)
 
 camera_fps = 30  # camera fps set to 30 by default can be changed to any other value
@@ -38,7 +38,7 @@ def prediction(current_frames, original_footage):
     current_frames = current_frames.reshape(1, 64, 224, 224, 5)
     prediction = model.predict(current_frames)
 
-    if prediction[0][0] > 0.5:  # Assuming model predicts as [Nonviolence,Violence]
+    if prediction[0][0] > 0.75:  # Assuming model predicts as [Nonviolence,Violence]
         global hit
         hit = True
         print("Hit")
